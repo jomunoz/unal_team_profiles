c26cc41cb3645a890845b402397940722283c4c6
Jorge Orlando Muñoz <jomunoz@gmail.com>
2014-03-02 17:44:11 -0500

    Change root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TeamProfiles::Application.routes.draw do
 
   resources :job_posts
+  root :to => 'job_posts#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
